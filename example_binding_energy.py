7894d29d5e2305c212d82e12ecd42d2e35a16708
Per Kaer <evilspaam@gmail.com>
2012-04-17 13:01:42 +0200

    oops now exmaple is added

---
+++ example_binding_energy.py
@@ -0,0 +1,33 @@
+import sys
+from scipy.sparse import csr_matrix as sm
+
+
+sys.path.insert(0, '.')
+
+import squanf_class
+
+Ne = 1
+Nh = 1
+
+eh = squanf_class.squanf(Ne=Ne, Nh=Nh, use_spin=True)
+
+# short hands
+
+e = eh.e
+eD = eh.eD
+h = eh.h
+hD = eh.hD
+
+# Hamiltonian
+
+H = sm(e(0, 'u').shape)
+
+Eex = 1.
+V1 = 20.
+V2 = 10.
+
+for spin in ['u', 'd']:
+    H = H + Eex / 2 * eD(0, spin=spin) * e(0, spin=spin)
+    H = H + Eex / 2 * hD(0, spin=spin) * h(0, spin=spin)
+
+print H.todense().astype(int)
